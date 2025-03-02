f43e13974f26d0e9b9bf80d785e862b59b9e0b23
SonuBhagat <bhagatji126@gmail.coml>
2025-03-03 00:08:21 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Education from './components/Education';
-import Work from './components/work';
+import Work from './components/Work';
 
 function App() {
   return (
