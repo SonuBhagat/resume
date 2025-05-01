3c14ac30c8418cca4696d946dcf6423c93f55c57
SonuBhagat <bhagatji126@gmail.coml>
2025-05-01 10:55:40 +0530

    Number change

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,10 +9,10 @@ export default function Contact() {
         <Mail className="w-5 h-5" />
         <span>{contactInfo.email}</span>
       </a>
-      <a href={`tel:${contactInfo.phone}`} className="flex text-lg items-center gap-2 hover:text-blue-200 transition-colors">
+      {/* <a href={`tel:${contactInfo.phone}`} className="flex text-lg items-center gap-2 hover:text-blue-200 transition-colors">
         <Phone className="w-5 h-5" />
         <span>{contactInfo.phone}</span>
-      </a>
+      </a> */}
       <div className="flex items-center text-lg gap-2">
         <MapPin className="w-5 h-5" />
         <span>{contactInfo.address}</span>
